de47eaea90f341b6401937883b7290c65c4e1db1
pianificazionemensa2025 <marcox.35@libero.it>
2025-03-27 18:14:18 -0500

    Update

---
+++ precache-manifest.d174fdd86f5dea4f6fda46f2f65a525e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd3badd638ba68d7c8d0155a5110b68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3324be9470ecb5fe582",
+    "url": "/static/css/main.17b58c61.chunk.css"
+  },
+  {
+    "revision": "b170e51db9b44490e59e",
+    "url": "/static/js/2.c8481a27.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/static/js/2.c8481a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3324be9470ecb5fe582",
+    "url": "/static/js/main.14b16cc8.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "2fa765956710ed75c20a04b60d0ae335",
+    "url": "/static/media/hashirshoaeb.2fa76595.png"
+  },
+  {
+    "revision": "68b329da9893e34099c7d8ad5cb9c940",
+    "url": "/static/media/resume.68b329da.pdf"
+  }
+]);
